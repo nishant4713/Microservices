f3d5da677a2500ad8d30bccf67688419ec2fc8a6
nishant4713 <108873242+nishant4713@users.noreply.github.com>
2025-07-15 12:20:36 +0530

    config server

--- Rating Service/Rating-Service/src/main/java/com/RatingService/RatingServiceApplication.java
+++ Rating Service/Rating-Service/src/main/java/com/RatingService/RatingServiceApplication.java
@@ -2,10 +2,11 @@ package com.RatingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
 public class RatingServiceApplication {
-
+@LoadBalanced
 	public static void main(String[] args) {
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
